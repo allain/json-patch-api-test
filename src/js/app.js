8870b92ffd10a8e68d1dd556de42b98cfe57c5ed
allain <allain.lalonde@gmail.com>
2014-12-28 12:27:30 -0500

    Making use of dom package isntead of basic dom operation

--- src/js/app.js
+++ src/js/app.js
@@ -4,9 +4,10 @@ var Store = require('./store.js');
 var store = new Store('test');
 var Handlebars = require('handlebars');
 var templates = require('./templates.js');
+var dom = require('dom');
 
-store.on('change', function(doc) {  
-  document.getElementById('app').innerText = templates.test({
+store.on('change', function(doc) {
+  dom('#app').text(templates.test({
     json: JSON.stringify(doc)
-  });
+  }));
 });
