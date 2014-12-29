daccddb86dc7dd4b2797bb19f43cfd140cab670b
allain <allain.lalonde@gmail.com>
2014-12-28 19:55:54 -0500

    Improving example

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
-    title: 'Express'
+    title: 'json-patch-api test',
+    host: 'http://' + req.host + ':3000'
   });
 });
 
--- src/js/app.js
+++ src/js/app.js
@@ -6,6 +6,6 @@ var JsonPatchApiClient = require('json-patch-api-client');
 var store = new JsonPatchApiClient(io, 'store1');
 store.on('change', function(doc) {
   dom('#app').text(templates.test({
-    json: JSON.stringify(doc)
+    json: JSON.stringify(doc, undefined, 2)
   }));
 });
